85a44ee71772d4c343a4946f5b8e466e91bf12f7
inna odina <odinainna@gmail.com>
2025-11-27 22:20:09 +0300

    modified some files, add new file, publish some staff

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+console.log("Welcome to the Brain Games!");
+export function runGame() {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  }
